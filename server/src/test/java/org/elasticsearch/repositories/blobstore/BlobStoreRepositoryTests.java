verawu/elasticsearch

commit f9b03d1a8d4630923ae220ee82409aef811d3cd5

Rework shard snapshot workers (#88209)
Currently, when starting the shard snapshot tasks, we loop through the
shards to be snapshotted back-to-back. This will calculate whether
there are changes in the shard to be snapshotted, write some metadata,
and ONLY if there are changes and there are files to be uploaded, fork
off the file upload calls. This could be further improved by
parallelizing shard snapshot tasks (currently, only file uploads happen
in parallel).

This change introduces a new type of task runner which uses a  queue to:

 1. Paralleize (with limited number of workers) shard snapshotting, and
    limit the number of the concurrently running snapshot tasks.
 2. Prioritize shard snapshot tasks over file snapshot tasks.

Closes https://github.com/elastic/elasticsearch/issues/83408

server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -150,6 +150,7 @@ import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import static org.elasticsearch.core.Strings.format;
+import static org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot.FileInfo;
 import static org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot.FileInfo.canonicalName;
 
 /**
@@ -376,6 +377,8 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
      */
     private final int maxSnapshotCount;
 
+    private final ShardSnapshotTaskRunner shardSnapshotTaskRunner;
+
     /**
      * Constructs new BlobStoreRepository
      * @param metadata   The metadata for this repository including name and settings
@@ -405,6 +408,12 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
         this.basePath = basePath;
         this.maxSnapshotCount = MAX_SNAPSHOTS_SETTING.get(metadata.settings());
         this.repoDataDeduplicator = new ResultDeduplicator<>(threadPool.getThreadContext());
+        shardSnapshotTaskRunner = new ShardSnapshotTaskRunner(
+            threadPool.info(ThreadPool.Names.SNAPSHOT).getMax(),
+            threadPool.executor(ThreadPool.Names.SNAPSHOT),
+            this::doSnapshotShard,
+            this::snapshotFile
+        );
     }
 
     @Override
@@ -2629,6 +2638,10 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
 
     @Override
     public void snapshotShard(SnapshotShardContext context) {
+        shardSnapshotTaskRunner.enqueueShardSnapshot(context);
+    }
+
+    private void doSnapshotShard(SnapshotShardContext context) {
         if (isReadOnly()) {
             context.onFailure(new RepositoryException(metadata.name(), "cannot snapshot shard on a readonly repository"));
             return;
@@ -2889,42 +2902,16 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
                 snapshotStatus.moveToDone(threadPool.absoluteTimeInMillis(), shardSnapshotResult);
                 context.onResponse(shardSnapshotResult);
             }, context::onFailure);
-            if (indexIncrementalFileCount == 0) {
+            if (indexIncrementalFileCount == 0 || filesToSnapshot.isEmpty()) {
                 allFilesUploadedListener.onResponse(Collections.emptyList());
                 return;
             }
-            final Executor executor = threadPool.executor(ThreadPool.Names.SNAPSHOT);
-            // Start as many workers as fit into the snapshot pool at once at the most
-            final int workers = Math.min(threadPool.info(ThreadPool.Names.SNAPSHOT).getMax(), indexIncrementalFileCount);
-            final ActionListener<Void> filesListener = fileQueueListener(filesToSnapshot, workers, allFilesUploadedListener);
-            for (int i = 0; i < workers; ++i) {
-                executeOneFileSnapshot(store, snapshotId, context.indexId(), snapshotStatus, filesToSnapshot, executor, filesListener);
+            final ActionListener<Void> filesListener = fileQueueListener(filesToSnapshot, filesToSnapshot.size(), allFilesUploadedListener);
+            for (FileInfo fileInfo : filesToSnapshot) {
+                shardSnapshotTaskRunner.enqueueFileSnapshot(context, fileInfo, filesListener);
             }
         } catch (Exception e) {
             context.onFailure(e);
-        }
-    }
-
-    private void executeOneFileSnapshot(
-        Store store,
-        SnapshotId snapshotId,
-        IndexId indexId,
-        IndexShardSnapshotStatus snapshotStatus,
-        BlockingQueue<BlobStoreIndexShardSnapshot.FileInfo> filesToSnapshot,
-        Executor executor,
-        ActionListener<Void> listener
-    ) throws InterruptedException {
-        final ShardId shardId = store.shardId();
-        final BlobStoreIndexShardSnapshot.FileInfo snapshotFileInfo = filesToSnapshot.poll(0L, TimeUnit.MILLISECONDS);
-        if (snapshotFileInfo == null) {
-            listener.onResponse(null);
-        } else {
-            executor.execute(ActionRunnable.wrap(listener, l -> {
-                try (Releasable ignored = incrementStoreRef(store, snapshotStatus, shardId)) {
-                    snapshotFile(snapshotFileInfo, indexId, shardId, snapshotId, snapshotStatus, store);
-                    executeOneFileSnapshot(store, snapshotId, indexId, snapshotStatus, filesToSnapshot, executor, l);
-                }
-            }));
         }
     }
 
@@ -3116,10 +3103,10 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
 
     private static ActionListener<Void> fileQueueListener(
         BlockingQueue<BlobStoreIndexShardSnapshot.FileInfo> files,
-        int workers,
+        int numberOfFiles,
         ActionListener<Collection<Void>> listener
     ) {
-        return new GroupedActionListener<>(listener, workers).delegateResponse((l, e) -> {
+        return new GroupedActionListener<>(listener, numberOfFiles).delegateResponse((l, e) -> {
             files.clear(); // Stop uploading the remaining files if we run into any exception
             l.onFailure(e);
         });
@@ -3426,19 +3413,20 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
 
     /**
      * Snapshot individual file
-     * @param fileInfo file to be snapshotted
+     * @param fileInfo file to snapshot
      */
-    private void snapshotFile(
-        BlobStoreIndexShardSnapshot.FileInfo fileInfo,
-        IndexId indexId,
-        ShardId shardId,
-        SnapshotId snapshotId,
-        IndexShardSnapshotStatus snapshotStatus,
-        Store store
-    ) throws IOException {
+    private void snapshotFile(SnapshotShardContext context, FileInfo fileInfo) throws IOException {
+        final IndexId indexId = context.indexId();
+        final Store store = context.store();
+        final ShardId shardId = store.shardId();
+        final IndexShardSnapshotStatus snapshotStatus = context.status();
+        final SnapshotId snapshotId = context.snapshotId();
         final BlobContainer shardContainer = shardContainer(indexId, shardId);
         final String file = fileInfo.physicalName();
-        try (IndexInput indexInput = store.openVerifyingInput(file, IOContext.READONCE, fileInfo.metadata())) {
+        try (
+            Releasable ignored = BlobStoreRepository.incrementStoreRef(store, snapshotStatus, store.shardId());
+            IndexInput indexInput = store.openVerifyingInput(file, IOContext.READONCE, fileInfo.metadata())
+        ) {
             for (int i = 0; i < fileInfo.numberOfParts(); i++) {
                 final long partBytes = fileInfo.partBytes(i);
 

server/src/main/java/org/elasticsearch/repositories/blobstore/ShardSnapshotTaskRunner.java
@@ -1,0 +1,140 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.repositories.blobstore;
+
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.ActionRunnable;
+import org.elasticsearch.common.CheckedBiConsumer;
+import org.elasticsearch.common.util.concurrent.PrioritizedThrottledTaskRunner;
+import org.elasticsearch.repositories.SnapshotShardContext;
+
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.function.Consumer;
+
+import static org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot.FileInfo;
+
+/**
+ * {@link ShardSnapshotTaskRunner} performs snapshotting tasks, prioritizing {@link ShardSnapshotTask}
+ * over {@link FileSnapshotTask}. Each enqueued shard to snapshot results in one {@link ShardSnapshotTask}
+ * and zero or more {@link FileSnapshotTask}s.
+ */
+public class ShardSnapshotTaskRunner {
+    private final PrioritizedThrottledTaskRunner<SnapshotTask> taskRunner;
+    private final Consumer<SnapshotShardContext> shardSnapshotter;
+    private final CheckedBiConsumer<SnapshotShardContext, FileInfo, IOException> fileSnapshotter;
+
+    abstract static class SnapshotTask implements Comparable<SnapshotTask>, Runnable {
+        protected final SnapshotShardContext context;
+
+        SnapshotTask(SnapshotShardContext context) {
+            this.context = context;
+        }
+
+        public abstract int priority();
+
+        public SnapshotShardContext context() {
+            return context;
+        }
+
+        @Override
+        public final int compareTo(SnapshotTask other) {
+            int res = Integer.compare(priority(), other.priority());
+            if (res != 0) {
+                return res;
+            }
+            return Long.compare(context.snapshotStartTime(), other.context.snapshotStartTime());
+        }
+    }
+
+    class ShardSnapshotTask extends SnapshotTask {
+        ShardSnapshotTask(SnapshotShardContext context) {
+            super(context);
+        }
+
+        @Override
+        public void run() {
+            shardSnapshotter.accept(context);
+        }
+
+        @Override
+        public int priority() {
+            return 1;
+        }
+
+        @Override
+        public String toString() {
+            return getClass().getSimpleName() + "{snapshotID=[" + context.snapshotId() + "], indexID=[" + context.indexId() + "]}";
+        }
+    }
+
+    class FileSnapshotTask extends SnapshotTask {
+        private final FileInfo fileInfo;
+        private final ActionListener<Void> fileSnapshotListener;
+
+        FileSnapshotTask(SnapshotShardContext context, FileInfo fileInfo, ActionListener<Void> fileSnapshotListener) {
+            super(context);
+            this.fileInfo = fileInfo;
+            this.fileSnapshotListener = fileSnapshotListener;
+        }
+
+        @Override
+        public void run() {
+            ActionRunnable.run(fileSnapshotListener, () -> fileSnapshotter.accept(context, fileInfo)).run();
+        }
+
+        @Override
+        public int priority() {
+            return 2;
+        }
+
+        @Override
+        public String toString() {
+            return getClass().getSimpleName()
+                + "{snapshotID=["
+                + context.snapshotId()
+                + "], indexID=["
+                + context.indexId()
+                + "], file=["
+                + fileInfo.name()
+                + "]}";
+        }
+    }
+
+    public ShardSnapshotTaskRunner(
+        final int maxRunningTasks,
+        final Executor executor,
+        final Consumer<SnapshotShardContext> shardSnapshotter,
+        final CheckedBiConsumer<SnapshotShardContext, FileInfo, IOException> fileSnapshotter
+    ) {
+        this.taskRunner = new PrioritizedThrottledTaskRunner<>("ShardSnapshotTaskRunner", maxRunningTasks, executor);
+        this.shardSnapshotter = shardSnapshotter;
+        this.fileSnapshotter = fileSnapshotter;
+    }
+
+    public void enqueueShardSnapshot(final SnapshotShardContext context) {
+        ShardSnapshotTask task = new ShardSnapshotTask(context);
+        taskRunner.enqueueTask(task);
+    }
+
+    public void enqueueFileSnapshot(final SnapshotShardContext context, final FileInfo fileInfo, final ActionListener<Void> listener) {
+        final FileSnapshotTask task = new FileSnapshotTask(context, fileInfo, listener);
+        taskRunner.enqueueTask(task);
+    }
+
+    // visible for testing
+    int runningTasks() {
+        return taskRunner.runningTasks();
+    }
+
+    // visible for testing
+    int queueSize() {
+        return taskRunner.queueSize();
+    }
+}

commit 3ea2fb9383901f4538b2dd1f77d8a4c6412ba390

Make sure listener is resolved when file queue is cleared (#89929)
This PR fixes a bug introduced in #88209 while refactoring how file upload
tasks run in a shard snapshot. The corner case where the queue of files
to snapshot gets cleared when a file snapshot runs into an exception is
not addressed in that PR.

Closes #89927
Closes #89956

server/src/main/java/org/elasticsearch/repositories/blobstore/BlobStoreRepository.java
@@ -2906,12 +2906,21 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
                 allFilesUploadedListener.onResponse(Collections.emptyList());
                 return;
             }
-            final ActionListener<Void> filesListener = fileQueueListener(filesToSnapshot, filesToSnapshot.size(), allFilesUploadedListener);
-            for (FileInfo fileInfo : filesToSnapshot) {
-                shardSnapshotTaskRunner.enqueueFileSnapshot(context, fileInfo, filesListener);
-            }
+            snapshotFiles(context, filesToSnapshot, allFilesUploadedListener);
         } catch (Exception e) {
             context.onFailure(e);
+        }
+    }
+
+    protected void snapshotFiles(
+        SnapshotShardContext context,
+        BlockingQueue<FileInfo> filesToSnapshot,
+        ActionListener<Collection<Void>> allFilesUploadedListener
+    ) {
+        final int noOfFilesToSnapshot = filesToSnapshot.size();
+        final ActionListener<Void> filesListener = fileQueueListener(filesToSnapshot, noOfFilesToSnapshot, allFilesUploadedListener);
+        for (int i = 0; i < noOfFilesToSnapshot; i++) {
+            shardSnapshotTaskRunner.enqueueFileSnapshot(context, filesToSnapshot::poll, filesListener);
         }
     }
 
@@ -3415,7 +3424,7 @@ public abstract class BlobStoreRepository extends AbstractLifecycleComponent imp
      * Snapshot individual file
      * @param fileInfo file to snapshot
      */
-    private void snapshotFile(SnapshotShardContext context, FileInfo fileInfo) throws IOException {
+    protected void snapshotFile(SnapshotShardContext context, FileInfo fileInfo) throws IOException {
         final IndexId indexId = context.indexId();
         final Store store = context.store();
         final ShardId shardId = store.shardId();

server/src/main/java/org/elasticsearch/repositories/blobstore/ShardSnapshotTaskRunner.java
@@ -17,6 +17,7 @@ import org.elasticsearch.repositories.SnapshotShardContext;
 import java.io.IOException;
 import java.util.concurrent.Executor;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import static org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot.FileInfo;
 
@@ -75,18 +76,23 @@ public class ShardSnapshotTaskRunner {
     }
 
     class FileSnapshotTask extends SnapshotTask {
-        private final FileInfo fileInfo;
+        private final Supplier<FileInfo> fileInfos;
         private final ActionListener<Void> fileSnapshotListener;
 
-        FileSnapshotTask(SnapshotShardContext context, FileInfo fileInfo, ActionListener<Void> fileSnapshotListener) {
+        FileSnapshotTask(SnapshotShardContext context, Supplier<FileInfo> fileInfos, ActionListener<Void> fileSnapshotListener) {
             super(context);
-            this.fileInfo = fileInfo;
+            this.fileInfos = fileInfos;
             this.fileSnapshotListener = fileSnapshotListener;
         }
 
         @Override
         public void run() {
-            ActionRunnable.run(fileSnapshotListener, () -> fileSnapshotter.accept(context, fileInfo)).run();
+            ActionRunnable.run(fileSnapshotListener, () -> {
+                FileInfo fileInfo = fileInfos.get();
+                if (fileInfo != null) {
+                    fileSnapshotter.accept(context, fileInfo);
+                }
+            }).run();
         }
 
         @Override
@@ -96,14 +102,7 @@ public class ShardSnapshotTaskRunner {
 
         @Override
         public String toString() {
-            return getClass().getSimpleName()
-                + "{snapshotID=["
-                + context.snapshotId()
-                + "], indexID=["
-                + context.indexId()
-                + "], file=["
-                + fileInfo.name()
-                + "]}";
+            return getClass().getSimpleName() + "{snapshotID=[" + context.snapshotId() + "], indexID=[" + context.indexId() + "]}";
         }
     }
 
@@ -123,8 +122,12 @@ public class ShardSnapshotTaskRunner {
         taskRunner.enqueueTask(task);
     }
 
-    public void enqueueFileSnapshot(final SnapshotShardContext context, final FileInfo fileInfo, final ActionListener<Void> listener) {
-        final FileSnapshotTask task = new FileSnapshotTask(context, fileInfo, listener);
+    public void enqueueFileSnapshot(
+        final SnapshotShardContext context,
+        final Supplier<FileInfo> fileInfos,
+        final ActionListener<Void> listener
+    ) {
+        final FileSnapshotTask task = new FileSnapshotTask(context, fileInfos, listener);
         taskRunner.enqueueTask(task);
     }
 

server/src/test/java/org/elasticsearch/repositories/blobstore/BlobStoreRepositoryTests.java
@@ -9,19 +9,25 @@
 package org.elasticsearch.repositories.blobstore;
 
 import org.elasticsearch.Version;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotResponse;
 import org.elasticsearch.action.support.PlainActionFuture;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.internal.Client;
 import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.cluster.metadata.RepositoryMetadata;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.Numbers;
 import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.util.BigArrays;
+import org.elasticsearch.common.util.MockBigArrays;
 import org.elasticsearch.env.Environment;
+import org.elasticsearch.env.TestEnvironment;
+import org.elasticsearch.index.snapshots.blobstore.BlobStoreIndexShardSnapshot;
 import org.elasticsearch.indices.recovery.RecoverySettings;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.RepositoryPlugin;
@@ -32,6 +38,7 @@ import org.elasticsearch.repositories.RepositoryData;
 import org.elasticsearch.repositories.RepositoryException;
 import org.elasticsearch.repositories.ShardGeneration;
 import org.elasticsearch.repositories.ShardGenerations;
+import org.elasticsearch.repositories.SnapshotShardContext;
 import org.elasticsearch.repositories.fs.FsRepository;
 import org.elasticsearch.snapshots.SnapshotId;
 import org.elasticsearch.snapshots.SnapshotState;
@@ -40,12 +47,15 @@ import org.elasticsearch.test.ESSingleNodeTestCase;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.xcontent.NamedXContentRegistry;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -393,4 +403,55 @@ public class BlobStoreRepositoryTests extends ESSingleNodeTestCase {
         return repoData;
     }
 
+    public void testEnsureUploadListenerIsResolvedWhenAFileSnapshotTaskFails() throws Exception {
+        Settings settings = Settings.builder().put("location", randomAlphaOfLength(10)).build();
+        RepositoryMetadata repositoryMetadata = new RepositoryMetadata(randomAlphaOfLength(10), FsRepository.TYPE, settings);
+        final ClusterService clusterService = BlobStoreTestUtil.mockClusterService(repositoryMetadata);
+        final FsRepository repository = new FsRepository(
+            repositoryMetadata,
+            createEnvironment(),
+            xContentRegistry(),
+            clusterService,
+            MockBigArrays.NON_RECYCLING_INSTANCE,
+            new RecoverySettings(Settings.EMPTY, new ClusterSettings(Settings.EMPTY, ClusterSettings.BUILT_IN_CLUSTER_SETTINGS))
+        ) {
+            @Override
+            protected void assertSnapshotOrGenericThread() {
+                // eliminate thread name check as we create repo manually
+            }
+
+            @Override
+            protected void snapshotFile(SnapshotShardContext context, BlobStoreIndexShardSnapshot.FileInfo fileInfo) throws IOException {
+                // Randomly fail some file snapshot tasks
+                if (randomBoolean()) {
+                    throw new IOException("cannot upload file");
+                }
+            }
+        };
+        clusterService.addStateApplier(event -> repository.updateState(event.state()));
+        // Apply state once to initialize repo properly like RepositoriesService would
+        repository.updateState(clusterService.state());
+        repository.start();
+        // Generate some FileInfo, as the files that get uploaded as part of the shard snapshot
+        SnapshotShardContext context = ShardSnapshotTaskRunnerTests.dummyContext();
+        int noOfFiles = randomIntBetween(10, 100);
+        BlockingQueue<BlobStoreIndexShardSnapshot.FileInfo> files = new LinkedBlockingQueue<>(noOfFiles);
+        PlainActionFuture<Void> listenerCalled = PlainActionFuture.newFuture();
+        ActionListener<Collection<Void>> allFilesUploadListener = ActionListener.wrap(() -> listenerCalled.onResponse(null));
+        for (int i = 0; i < noOfFiles; i++) {
+            files.add(ShardSnapshotTaskRunnerTests.dummyFileInfo());
+        }
+        repository.snapshotFiles(context, files, allFilesUploadListener);
+        listenerCalled.get();
+    }
+
+    private Environment createEnvironment() {
+        Path home = createTempDir();
+        return TestEnvironment.newEnvironment(
+            Settings.builder()
+                .put(Environment.PATH_HOME_SETTING.getKey(), home.toAbsolutePath())
+                .put(Environment.PATH_REPO_SETTING.getKey(), home.resolve("repo").toAbsolutePath())
+                .build()
+        );
+    }
 }

server/src/test/java/org/elasticsearch/repositories/blobstore/ShardSnapshotTaskRunnerTests.java
@@ -78,7 +78,7 @@ public class ShardSnapshotTaskRunnerTests extends ESTestCase {
                     filesToUpload
                 );
                 for (int i = 0; i < filesToUpload; i++) {
-                    taskRunner.enqueueFileSnapshot(context, dummyFileInfo(), uploadListener);
+                    taskRunner.enqueueFileSnapshot(context, ShardSnapshotTaskRunnerTests::dummyFileInfo, uploadListener);
                 }
             }
             finishedShardSnapshotTasks.incrementAndGet();
@@ -105,17 +105,17 @@ public class ShardSnapshotTaskRunnerTests extends ESTestCase {
         }
     }
 
-    private static BlobStoreIndexShardSnapshot.FileInfo dummyFileInfo() {
+    public static BlobStoreIndexShardSnapshot.FileInfo dummyFileInfo() {
         String filename = randomAlphaOfLength(10);
         StoreFileMetadata metadata = new StoreFileMetadata(filename, 10, "CHECKSUM", Version.CURRENT.luceneVersion.toString());
         return new BlobStoreIndexShardSnapshot.FileInfo(filename, metadata, null);
     }
 
-    private SnapshotShardContext dummyContext() {
+    public static SnapshotShardContext dummyContext() {
         return dummyContext(new SnapshotId(randomAlphaOfLength(10), UUIDs.randomBase64UUID()), randomMillisUpToYear9999());
     }
 
-    private SnapshotShardContext dummyContext(final SnapshotId snapshotId, final long startTime) {
+    public static SnapshotShardContext dummyContext(final SnapshotId snapshotId, final long startTime) {
         IndexId indexId = new IndexId(randomAlphaOfLength(10), UUIDs.randomBase64UUID());
         ShardId shardId = new ShardId(indexId.getName(), indexId.getId(), 1);
         Settings settings = Settings.builder()
@@ -177,17 +177,21 @@ public class ShardSnapshotTaskRunnerTests extends ESTestCase {
         assertThat(workers.new ShardSnapshotTask(s2Context).compareTo(workers.new ShardSnapshotTask(s3Context)), equalTo(0));
         // Shard snapshot task always has a higher priority over file snapshot
         assertThat(
-            workers.new ShardSnapshotTask(s1Context).compareTo(workers.new FileSnapshotTask(s1Context, dummyFileInfo(), listener)),
+            workers.new ShardSnapshotTask(s1Context).compareTo(
+                workers.new FileSnapshotTask(s1Context, ShardSnapshotTaskRunnerTests::dummyFileInfo, listener)
+            ),
             lessThan(0)
         );
         assertThat(
-            workers.new ShardSnapshotTask(s2Context).compareTo(workers.new FileSnapshotTask(s1Context, dummyFileInfo(), listener)),
+            workers.new ShardSnapshotTask(s2Context).compareTo(
+                workers.new FileSnapshotTask(s1Context, ShardSnapshotTaskRunnerTests::dummyFileInfo, listener)
+            ),
             lessThan(0)
         );
         // File snapshots are prioritized by start time.
         assertThat(
-            workers.new FileSnapshotTask(s1Context, dummyFileInfo(), listener).compareTo(
-                workers.new FileSnapshotTask(s2Context, dummyFileInfo(), listener)
+            workers.new FileSnapshotTask(s1Context, ShardSnapshotTaskRunnerTests::dummyFileInfo, listener).compareTo(
+                workers.new FileSnapshotTask(s2Context, ShardSnapshotTaskRunnerTests::dummyFileInfo, listener)
             ),
             lessThan(0)
         );

test/framework/src/main/java/org/elasticsearch/test/ESTestCase.java
@@ -1018,7 +1018,7 @@ public abstract class ESTestCase extends LuceneTestCase {
     /**
      * generate a random epoch millis in a range 1 to 9999-12-31T23:59:59.999
      */
-    public long randomMillisUpToYear9999() {
+    public static long randomMillisUpToYear9999() {
         return randomLongBetween(1, DateUtils.MAX_MILLIS_BEFORE_9999);
     }
 