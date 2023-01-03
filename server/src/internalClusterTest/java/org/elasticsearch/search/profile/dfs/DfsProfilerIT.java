verawu/elasticsearch

commit 8b0d0716d1db31a596183731d3a0b7b691d305e2

Add profiling and documentation for dfs phase (#90536)
Adds profiling statistics for the dfs phase, and adds documentation for both the dfs phase profiling 
and kNN profiling.

Closes #89713

server/src/main/java/org/elasticsearch/search/dfs/DfsPhase.java
@@ -16,9 +16,12 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.search.TermStatistics;
 import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.search.Weight;
 import org.elasticsearch.index.query.SearchExecutionContext;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.internal.SearchContext;
+import org.elasticsearch.search.profile.dfs.DfsProfiler;
+import org.elasticsearch.search.profile.dfs.DfsTimingType;
 import org.elasticsearch.search.profile.query.CollectorResult;
 import org.elasticsearch.search.profile.query.InternalProfileCollector;
 import org.elasticsearch.search.rescore.RescoreContext;
@@ -44,12 +47,18 @@ public class DfsPhase {
         try {
             collectStatistics(context);
             executeKnnVectorQuery(context);
+
+            if (context.getProfilers() != null) {
+                context.dfsResult().profileResult(context.getProfilers().getDfsProfiler().buildDfsPhaseResults());
+            }
         } catch (Exception e) {
             throw new DfsPhaseExecutionException(context.shardTarget(), "Exception during dfs phase", e);
         }
     }
 
     private void collectStatistics(SearchContext context) throws IOException {
+        final DfsProfiler profiler = context.getProfilers() == null ? null : context.getProfilers().getDfsProfiler();
+
         Map<String, CollectionStatistics> fieldStatistics = new HashMap<>();
         Map<Term, TermStatistics> stats = new HashMap<>();
 
@@ -59,11 +68,22 @@ public class DfsPhase {
                 if (context.isCancelled()) {
                     throw new TaskCancelledException("cancelled");
                 }
-                TermStatistics ts = super.termStatistics(term, docFreq, totalTermFreq);
-                if (ts != null) {
-                    stats.put(term, ts);
+
+                if (profiler != null) {
+                    profiler.startTimer(DfsTimingType.TERM_STATISTICS);
                 }
-                return ts;
+
+                try {
+                    TermStatistics ts = super.termStatistics(term, docFreq, totalTermFreq);
+                    if (ts != null) {
+                        stats.put(term, ts);
+                    }
+                    return ts;
+                } finally {
+                    if (profiler != null) {
+                        profiler.stopTimer(DfsTimingType.TERM_STATISTICS);
+                    }
+                }
             }
 
             @Override
@@ -71,18 +91,69 @@ public class DfsPhase {
                 if (context.isCancelled()) {
                     throw new TaskCancelledException("cancelled");
                 }
-                CollectionStatistics cs = super.collectionStatistics(field);
-                if (cs != null) {
-                    fieldStatistics.put(field, cs);
+
+                if (profiler != null) {
+                    profiler.startTimer(DfsTimingType.COLLECTION_STATISTICS);
                 }
-                return cs;
+
+                try {
+                    CollectionStatistics cs = super.collectionStatistics(field);
+                    if (cs != null) {
+                        fieldStatistics.put(field, cs);
+                    }
+                    return cs;
+                } finally {
+                    if (profiler != null) {
+                        profiler.stopTimer(DfsTimingType.COLLECTION_STATISTICS);
+                    }
+                }
+            }
+
+            @Override
+            public Weight createWeight(Query query, ScoreMode scoreMode, float boost) throws IOException {
+                if (profiler != null) {
+                    profiler.startTimer(DfsTimingType.CREATE_WEIGHT);
+                }
+
+                try {
+                    return super.createWeight(query, scoreMode, boost);
+                } finally {
+                    if (profiler != null) {
+                        profiler.stopTimer(DfsTimingType.CREATE_WEIGHT);
+                    }
+                }
+            }
+
+            @Override
+            public Query rewrite(Query original) throws IOException {
+                if (profiler != null) {
+                    profiler.startTimer(DfsTimingType.REWRITE);
+                }
+
+                try {
+                    return super.rewrite(original);
+                } finally {
+                    if (profiler != null) {
+                        profiler.stopTimer(DfsTimingType.REWRITE);
+                    }
+                }
             }
         };
 
-        searcher.createWeight(context.rewrittenQuery(), ScoreMode.COMPLETE, 1);
-        for (RescoreContext rescoreContext : context.rescore()) {
-            for (Query query : rescoreContext.getQueries()) {
-                searcher.createWeight(context.searcher().rewrite(query), ScoreMode.COMPLETE, 1);
+        if (profiler != null) {
+            profiler.start();
+        }
+
+        try {
+            searcher.createWeight(context.rewrittenQuery(), ScoreMode.COMPLETE, 1);
+            for (RescoreContext rescoreContext : context.rescore()) {
+                for (Query query : rescoreContext.getQueries()) {
+                    searcher.createWeight(searcher.rewrite(query), ScoreMode.COMPLETE, 1);
+                }
+            }
+        } finally {
+            if (profiler != null) {
+                profiler.stop();
             }
         }
 
@@ -122,16 +193,12 @@ public class DfsPhase {
                 CollectorResult.REASON_SEARCH_TOP_HITS,
                 List.of()
             );
-            context.getProfilers().getCurrentQueryProfiler().setCollector(ipc);
+            context.getProfilers().getDfsProfiler().setCollector(ipc);
             collector = ipc;
         }
 
         context.searcher().search(query, collector);
         DfsKnnResults knnResults = new DfsKnnResults(topScoreDocCollector.topDocs().scoreDocs);
         context.dfsResult().knnResults(knnResults);
-
-        if (context.getProfilers() != null) {
-            context.dfsResult().profileResult(context.getProfilers().buildDfsPhaseResults());
-        }
     }
 }

server/src/main/java/org/elasticsearch/search/profile/SearchProfileDfsPhaseResult.java
@@ -27,22 +27,27 @@ import static org.elasticsearch.xcontent.ConstructingObjectParser.optionalConstr
 
 public class SearchProfileDfsPhaseResult implements Writeable, ToXContentObject {
 
+    private final ProfileResult dfsShardResult;
     private final QueryProfileShardResult queryProfileShardResult;
 
     @ParserConstructor
-    public SearchProfileDfsPhaseResult(@Nullable QueryProfileShardResult queryProfileShardResult) {
+    public SearchProfileDfsPhaseResult(@Nullable ProfileResult dfsShardResult, @Nullable QueryProfileShardResult queryProfileShardResult) {
+        this.dfsShardResult = dfsShardResult;
         this.queryProfileShardResult = queryProfileShardResult;
     }
 
     public SearchProfileDfsPhaseResult(StreamInput in) throws IOException {
+        dfsShardResult = in.readOptionalWriteable(ProfileResult::new);
         queryProfileShardResult = in.readOptionalWriteable(QueryProfileShardResult::new);
     }
 
     @Override
     public void writeTo(StreamOutput out) throws IOException {
+        out.writeOptionalWriteable(dfsShardResult);
         out.writeOptionalWriteable(queryProfileShardResult);
     }
 
+    private static final ParseField STATISTICS = new ParseField("statistics");
     private static final ParseField KNN = new ParseField("knn");
     private static final InstantiatingObjectParser<SearchProfileDfsPhaseResult, Void> PARSER;
 
@@ -52,6 +57,7 @@ public class SearchProfileDfsPhaseResult implements Writeable, ToXContentObject 
             true,
             SearchProfileDfsPhaseResult.class
         );
+        parser.declareObject(optionalConstructorArg(), (p, c) -> ProfileResult.fromXContent(p), STATISTICS);
         parser.declareObject(optionalConstructorArg(), (p, c) -> QueryProfileShardResult.fromXContent(p), KNN);
         PARSER = parser.build();
     }
@@ -63,6 +69,10 @@ public class SearchProfileDfsPhaseResult implements Writeable, ToXContentObject 
     @Override
     public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
         builder.startObject();
+        if (dfsShardResult != null) {
+            builder.field(STATISTICS.getPreferredName());
+            dfsShardResult.toXContent(builder, params);
+        }
         if (queryProfileShardResult != null) {
             builder.field(KNN.getPreferredName());
             queryProfileShardResult.toXContent(builder, params);
@@ -76,11 +86,21 @@ public class SearchProfileDfsPhaseResult implements Writeable, ToXContentObject 
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SearchProfileDfsPhaseResult that = (SearchProfileDfsPhaseResult) o;
-        return Objects.equals(queryProfileShardResult, that.queryProfileShardResult);
+        return Objects.equals(dfsShardResult, that.dfsShardResult) && Objects.equals(queryProfileShardResult, that.queryProfileShardResult);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(queryProfileShardResult);
+        return Objects.hash(dfsShardResult, queryProfileShardResult);
+    }
+
+    @Override
+    public String toString() {
+        return "SearchProfileDfsPhaseResult{"
+            + "dfsShardResult="
+            + dfsShardResult
+            + ", queryProfileShardResult="
+            + queryProfileShardResult
+            + '}';
     }
 }

commit 084ca74fb23a7d4c951ba2db4bc3319ca90fcad3

Fix timing bug with DFS profiling (#92421)
Introduced in: https://github.com/elastic/elasticsearch/pull/90536

Profiling for DFS has had its timing numbers looking weird, additionally, it would trigger some assertion failures because `timer.start()` was called without a `stop()` in between.

The key issue was around query `weight` creation. `Weight` creation could be called recursively, thus calling `start` on the timer more than once before calling stop.

server/src/main/java/org/elasticsearch/search/dfs/DfsPhase.java
@@ -16,7 +16,6 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.search.TermStatistics;
 import org.apache.lucene.search.TopScoreDocCollector;
-import org.apache.lucene.search.Weight;
 import org.elasticsearch.index.query.SearchExecutionContext;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.internal.SearchContext;
@@ -33,6 +32,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  * DFS phase of a search request, used to make scoring 100% accurate by collecting additional info from each shard before the query phase.
@@ -61,6 +61,16 @@ public class DfsPhase {
 
         Map<String, CollectionStatistics> fieldStatistics = new HashMap<>();
         Map<Term, TermStatistics> stats = new HashMap<>();
+        final Consumer<DfsTimingType> maybeStart = dtt -> {
+            if (profiler != null) {
+                profiler.startTimer(dtt);
+            }
+        };
+        final Consumer<DfsTimingType> maybeStop = dtt -> {
+            if (profiler != null) {
+                profiler.stopTimer(dtt);
+            }
+        };
 
         IndexSearcher searcher = new IndexSearcher(context.searcher().getIndexReader()) {
             @Override
@@ -68,11 +78,7 @@ public class DfsPhase {
                 if (context.isCancelled()) {
                     throw new TaskCancelledException("cancelled");
                 }
-
-                if (profiler != null) {
-                    profiler.startTimer(DfsTimingType.TERM_STATISTICS);
-                }
-
+                maybeStart.accept(DfsTimingType.TERM_STATISTICS);
                 try {
                     TermStatistics ts = super.termStatistics(term, docFreq, totalTermFreq);
                     if (ts != null) {
@@ -80,9 +86,7 @@ public class DfsPhase {
                     }
                     return ts;
                 } finally {
-                    if (profiler != null) {
-                        profiler.stopTimer(DfsTimingType.TERM_STATISTICS);
-                    }
+                    maybeStop.accept(DfsTimingType.TERM_STATISTICS);
                 }
             }
 
@@ -91,11 +95,7 @@ public class DfsPhase {
                 if (context.isCancelled()) {
                     throw new TaskCancelledException("cancelled");
                 }
-
-                if (profiler != null) {
-                    profiler.startTimer(DfsTimingType.COLLECTION_STATISTICS);
-                }
-
+                maybeStart.accept(DfsTimingType.COLLECTION_STATISTICS);
                 try {
                     CollectionStatistics cs = super.collectionStatistics(field);
                     if (cs != null) {
@@ -103,39 +103,7 @@ public class DfsPhase {
                     }
                     return cs;
                 } finally {
-                    if (profiler != null) {
-                        profiler.stopTimer(DfsTimingType.COLLECTION_STATISTICS);
-                    }
-                }
-            }
-
-            @Override
-            public Weight createWeight(Query query, ScoreMode scoreMode, float boost) throws IOException {
-                if (profiler != null) {
-                    profiler.startTimer(DfsTimingType.CREATE_WEIGHT);
-                }
-
-                try {
-                    return super.createWeight(query, scoreMode, boost);
-                } finally {
-                    if (profiler != null) {
-                        profiler.stopTimer(DfsTimingType.CREATE_WEIGHT);
-                    }
-                }
-            }
-
-            @Override
-            public Query rewrite(Query original) throws IOException {
-                if (profiler != null) {
-                    profiler.startTimer(DfsTimingType.REWRITE);
-                }
-
-                try {
-                    return super.rewrite(original);
-                } finally {
-                    if (profiler != null) {
-                        profiler.stopTimer(DfsTimingType.REWRITE);
-                    }
+                    maybeStop.accept(DfsTimingType.COLLECTION_STATISTICS);
                 }
             }
         };
@@ -145,10 +113,27 @@ public class DfsPhase {
         }
 
         try {
-            searcher.createWeight(context.rewrittenQuery(), ScoreMode.COMPLETE, 1);
+            try {
+                maybeStart.accept(DfsTimingType.CREATE_WEIGHT);
+                searcher.createWeight(context.rewrittenQuery(), ScoreMode.COMPLETE, 1);
+            } finally {
+                maybeStop.accept(DfsTimingType.CREATE_WEIGHT);
+            }
             for (RescoreContext rescoreContext : context.rescore()) {
                 for (Query query : rescoreContext.getQueries()) {
-                    searcher.createWeight(searcher.rewrite(query), ScoreMode.COMPLETE, 1);
+                    final Query rewritten;
+                    try {
+                        maybeStart.accept(DfsTimingType.REWRITE);
+                        rewritten = searcher.rewrite(query);
+                    } finally {
+                        maybeStop.accept(DfsTimingType.REWRITE);
+                    }
+                    try {
+                        maybeStart.accept(DfsTimingType.CREATE_WEIGHT);
+                        searcher.createWeight(rewritten, ScoreMode.COMPLETE, 1);
+                    } finally {
+                        maybeStop.accept(DfsTimingType.CREATE_WEIGHT);
+                    }
                 }
             }
         } finally {

server/src/main/java/org/elasticsearch/search/profile/SearchProfileDfsPhaseResult.java
@@ -103,4 +103,12 @@ public class SearchProfileDfsPhaseResult implements Writeable, ToXContentObject 
             + queryProfileShardResult
             + '}';
     }
+
+    public ProfileResult getDfsShardResult() {
+        return dfsShardResult;
+    }
+
+    public QueryProfileShardResult getQueryProfileShardResult() {
+        return queryProfileShardResult;
+    }
 }

server/src/internalClusterTest/java/org/elasticsearch/search/profile/dfs/DfsProfilerIT.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+package org.elasticsearch.search.profile.dfs;
+
+import org.apache.lucene.tests.util.English;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.profile.ProfileResult;
+import org.elasticsearch.search.profile.SearchProfileDfsPhaseResult;
+import org.elasticsearch.search.profile.SearchProfileShardResult;
+import org.elasticsearch.search.profile.query.CollectorResult;
+import org.elasticsearch.search.profile.query.QueryProfileShardResult;
+import org.elasticsearch.search.vectors.KnnSearchBuilder;
+import org.elasticsearch.test.ESIntegTestCase;
+import org.elasticsearch.xcontent.XContentFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.elasticsearch.search.profile.query.RandomQueryGenerator.randomQueryBuilder;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class DfsProfilerIT extends ESIntegTestCase {
+
+    private static final int KNN_DIM = 3;
+
+    public void testProfileDfs() throws Exception {
+        String textField = "text_field";
+        String numericField = "number";
+        String vectorField = "vector";
+        String indexName = "text-dfs-profile";
+        createIndex(indexName, vectorField);
+        ensureGreen();
+
+        int numDocs = randomIntBetween(10, 50);
+        IndexRequestBuilder[] docs = new IndexRequestBuilder[numDocs];
+        for (int i = 0; i < numDocs; i++) {
+            docs[i] = client().prepareIndex(indexName)
+                .setId(String.valueOf(i))
+                .setSource(
+                    textField,
+                    English.intToEnglish(i),
+                    numericField,
+                    i,
+                    vectorField,
+                    new float[] { randomFloat(), randomFloat(), randomFloat() }
+                );
+        }
+        indexRandom(true, docs);
+        refresh();
+        int iters = between(5, 10);
+        for (int i = 0; i < iters; i++) {
+            QueryBuilder q = randomQueryBuilder(List.of(textField), List.of(numericField), numDocs, 3);
+            logger.info("Query: {}", q);
+
+            SearchResponse resp = client().prepareSearch()
+                .setQuery(q)
+                .setTrackTotalHits(true)
+                .setProfile(true)
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setKnnSearch(new KnnSearchBuilder(vectorField, new float[] { randomFloat(), randomFloat(), randomFloat() }, 5, 50))
+                .get();
+
+            assertNotNull("Profile response element should not be null", resp.getProfileResults());
+            assertThat("Profile response should not be an empty array", resp.getProfileResults().size(), not(0));
+            for (Map.Entry<String, SearchProfileShardResult> shard : resp.getProfileResults().entrySet()) {
+                for (QueryProfileShardResult searchProfiles : shard.getValue().getQueryProfileResults()) {
+                    for (ProfileResult result : searchProfiles.getQueryResults()) {
+                        assertNotNull(result.getQueryName());
+                        assertNotNull(result.getLuceneDescription());
+                        assertThat(result.getTime(), greaterThan(0L));
+                    }
+                    CollectorResult result = searchProfiles.getCollectorResult();
+                    assertThat(result.getName(), is(not(emptyOrNullString())));
+                    assertThat(result.getTime(), greaterThan(0L));
+                }
+                SearchProfileDfsPhaseResult searchProfileDfsPhaseResult = shard.getValue().getSearchProfileDfsPhaseResult();
+                assertThat(searchProfileDfsPhaseResult, is(notNullValue()));
+                for (ProfileResult result : searchProfileDfsPhaseResult.getQueryProfileShardResult().getQueryResults()) {
+                    assertNotNull(result.getQueryName());
+                    assertNotNull(result.getLuceneDescription());
+                    assertThat(result.getTime(), greaterThan(0L));
+                }
+                CollectorResult result = searchProfileDfsPhaseResult.getQueryProfileShardResult().getCollectorResult();
+                assertThat(result.getName(), is(not(emptyOrNullString())));
+                assertThat(result.getTime(), greaterThan(0L));
+                ProfileResult statsResult = searchProfileDfsPhaseResult.getDfsShardResult();
+                assertThat(statsResult.getQueryName(), equalTo("statistics"));
+                assertThat(result.getTime(), greaterThan(0L));
+            }
+        }
+    }
+
+    private void createIndex(String name, String vectorField) throws IOException {
+        assertAcked(
+            prepareCreate(name).setMapping(
+                XContentFactory.jsonBuilder()
+                    .startObject()
+                    .startObject("properties")
+                    .startObject(vectorField)
+                    .field("type", "dense_vector")
+                    .field("dims", KNN_DIM)
+                    .field("index", true)
+                    .field("similarity", "cosine")
+                    .endObject()
+                    .endObject()
+                    .endObject()
+            )
+        );
+    }
+
+}